032b78c86d3d262b85e947cbf42834e0b38691d1
yesa111 <2ysufesaydn2@gmail.com>
2024-10-26 15:11:03 +0300

    5

--- src/game.js
+++ src/game.js
@@ -862,7 +862,7 @@ var my_body;
 let texture;
 
 function preload() {
-    texture = loadImage('src/insta.png'); // Doku dosyasını yükleyin
+    texture = loadImage('C:/Users/Acer/Desktop/doom/src/insta.png'); // Doku dosyasını yükleyin
 }
 
 function setup_game() {
@@ -907,7 +907,6 @@ function setup_game() {
             [.1, .1, .1]
         ))
     );
-}
 
     range(1).map(x => {
         chaingun.push(new Sprite(
